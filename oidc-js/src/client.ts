f4d2293345a512b7f05ea2ec06fcba4ef2f42493
Thivi <theviyanthan@outlook.com>
2020-09-23 15:47:37 +0530

    Add getDocodedToken method

--- oidc-js/src/client.ts
+++ oidc-js/src/client.ts
@@ -22,15 +22,17 @@ import { AxiosHttpClient, AxiosHttpClientInstance } from "./http-client";
 import {
     ConfigInterface,
     CustomGrantRequestParams,
+    DecodedIdTokenPayloadInterface,
+    isWebWorkerConfig,
     ServiceResourcesType,
     UserInfo,
     WebWorkerClientInterface,
-    WebWorkerConfigInterface,
-    isWebWorkerConfig
+    WebWorkerConfigInterface
 } from "./models";
 import {
     customGrant as customGrantUtil,
     endAuthenticatedSession,
+    getDecodedIDToken,
     getServiceEndpoints,
     getSessionParameter,
     getUserInfo as getUserInfoUtil,
@@ -350,6 +352,14 @@ export class IdentityClient {
         }
 
         throw Error("Identity Client has not been initialized yet");
+    }
+
+    public getDecodedIDToken(): Promise<DecodedIdTokenPayloadInterface>{
+        if (this._storage === Storage.WebWorker) {
+            return this._client.getDecodedIDToken();
+        }
+
+        return Promise.resolve(getDecodedIDToken(this._authConfig));
     }
 
     public on(hook: Hooks.CustomGrant, callback: (response?: any) => void, id: string): void;
