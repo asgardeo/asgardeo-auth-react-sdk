e73a9bf1096f466a910499bc5c3e7d22f6c7471c
Thivi <theviyanthan@outlook.com>
2020-09-24 22:17:57 +0530

    Add logout success message.

--- oidc-js/src/constants/messages.ts
+++ oidc-js/src/constants/messages.ts
@@ -34,3 +34,4 @@ export const REQUEST_FINISH = "request-finish";
 export const GET_SERVICE_ENDPOINTS = "get-service-endpoints";
 export const GET_USER_INFO = "get-user-info";
 export const GET_DECODED_ID_TOKEN = "get-decoded-id-token";
+export const LOGOUT_SUCCESS = "logout_success";
