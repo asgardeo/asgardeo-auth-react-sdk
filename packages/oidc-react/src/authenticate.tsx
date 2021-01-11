8ab0aa9239d3c1343e6273dd5345b5fb828f8ffd
Thivi <theviyanthan@outlook.com>
2021-01-11 23:13:27 +0530

    Add all APIs to the provider

--- packages/oidc-react/src/authenticate.tsx
+++ packages/oidc-react/src/authenticate.tsx
@@ -16,6 +16,15 @@
  * under the License.
  */
 
+import {
+    AuthClientConfig,
+    BasicUserInfo,
+    Config,
+    CustomGrantConfig,
+    Hooks,
+    HttpRequestConfig,
+    HttpResponse
+} from "@asgardeo/auth-spa";
 import React, { FunctionComponent, createContext, useContext, useEffect, useState } from "react";
 import AuthAPI from "./api";
 import { AuthContextInterface } from "./models";
@@ -26,41 +35,86 @@ const AuthClient = new AuthAPI();
  * Authentication Context to hold global states in react components.
  */
 const AuthContext = createContext<AuthContextInterface>({
-    signIn: () => { return; },
-    signOut: () => { return; },
+    ...AuthClient,
     state: AuthClient.getState()
 });
 
 const AuthProvider: FunctionComponent<{ children: any; config: any }> = ({ children, config }) => {
+    const [state, dispatch] = useState(AuthClient.getState());
+    const [configState, setConfigState] = useState(null);
 
-    const [ state, dispatch ] = useState(AuthClient.getState());
-    const [ configState, setConfigState ] = useState(null);
+    const signIn = (callback?) => {
+        AuthClient.signIn(dispatch, state, callback);
+    };
+    const signOut = (callback?) => {
+        AuthClient.signOut(dispatch, state, callback);
+    };
+    const getBasicUserInfo = () => AuthClient.getBasicUserInfo();
+    const httpRequest = (config: HttpRequestConfig) => AuthClient.httpRequest(config);
+    const httpRequestAll = (configs: HttpRequestConfig[]) => AuthClient.httpRequestAll(configs);
+    const requestCustomGrant = (
+        config: CustomGrantConfig,
+        callback: (response: BasicUserInfo | HttpResponse<any>) => void
+    ) => AuthClient.requestCustomGrant(config, callback, dispatch);
+    const revokeAccessToken = () => AuthClient.revokeAccessToken(dispatch);
+    const getOIDCServiceEndpoints = () => AuthClient.getOIDCServiceEndpoints();
+    const getHttpClient = () => AuthClient.getHttpClient();
+    const getDecodedIDToken = () => AuthClient.getDecodedIDToken();
+    const getAccessToken = () => AuthClient.getAccessToken();
+    const refreshAccessToken = () => AuthClient.refreshAccessToken();
+    const isAuthenticated = () => AuthClient.isAuthenticated();
+    const enableHttpHandler = () => AuthClient.enableHttpHandler();
+    const disableHttpHandler = () => AuthClient.disableHttpHandler();
+    const updateConfig = (config: Partial<AuthClientConfig<Config>>) => AuthClient.updateConfig(config);
+    const on = (hook: Hooks, callback: (response?: any) => void, id?: string) => {
+        if (hook === Hooks.CustomGrant) {
+            return AuthClient.on(hook, callback, id);
+        }
 
-    const signIn = (callback?) => { AuthClient.signIn(dispatch, state, callback); };
-    const signOut = (callback?) => { AuthClient.signOut(dispatch, state, callback); };
+        return AuthClient.on(hook, callback);
+    };
 
     useEffect(() => {
-
         if (state.isAuthenticated) {
             return;
         }
 
         AuthClient.init(config);
         setConfigState(config);
-
     }, [config]);
 
     /**
      * Render state and special case actions
      */
     return (
-        <AuthContext.Provider value={ { signIn, signOut, state } }>
-            { configState && children }
+        <AuthContext.Provider
+            value={{
+                disableHttpHandler,
+                enableHttpHandler,
+                getAccessToken,
+                getBasicUserInfo,
+                getDecodedIDToken,
+                getHttpClient,
+                getOIDCServiceEndpoints,
+                httpRequest,
+                httpRequestAll,
+                isAuthenticated,
+                on,
+                refreshAccessToken,
+                requestCustomGrant,
+                revokeAccessToken,
+                signIn,
+                signOut,
+                state,
+                updateConfig
+            }}
+        >
+            {configState && children}
         </AuthContext.Provider>
     );
 };
 
-const useAuthContext = () => {
+const useAuthContext = (): (() => void) => {
     return useContext(AuthContext);
 };
 
