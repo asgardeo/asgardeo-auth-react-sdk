9141848cf84c8e37b1f40420f74cc82caf3dab06
Thivi <theviyanthan@outlook.com>
2021-01-18 22:11:26 +0530

    Move typescript sample to the sample root

--- lib/src/auth-spa.d.ts
+++ lib/src/auth-spa.d.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -15,4 +15,5 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-export * from "./atom";
+
+export * from "@asgardeo/auth-spa";
--- lib/src/polyfilled.ts
+++ lib/src/polyfilled.ts
@@ -1,5 +1,5 @@
 /**
-* Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+* Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
 *
 * WSO2 Inc. licenses this file to you under the Apache License,
 * Version 2.0 (the "License"); you may not use this file except
@@ -16,4 +16,7 @@
 * under the License.
 */
 
-export * from "./route-paths";
+process.env.polyfilled = "true";
+
+export * from "@asgardeo/auth-spa";
+export * from "./public-api";
