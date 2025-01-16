8fd95e1ca025c3b69513c2a2384aff5003c1baec
Pavindu Lakshan <pavindulakshan@gmail.com>
2025-01-16 19:14:37 +0530

    Update indentation

--- samples/asgardeo-react-app/src/app.tsx
+++ samples/asgardeo-react-app/src/app.tsx
@@ -17,7 +17,7 @@
  */
 
 import { AuthProvider, useAuthContext } from "@asgardeo/auth-react";
-import config from "./config.json";
+import {default as authConfig } from "./config.json";
 import { FunctionComponent, ReactElement } from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { ErrorBoundary } from "./error-boundary";
@@ -25,26 +25,26 @@ import { HomePage } from "./pages/home";
 import { NotFoundPage } from "./pages/404";
 
 const AppContent: FunctionComponent = (): ReactElement => {
-  const { error } = useAuthContext();
+    const { error } = useAuthContext();
 
-  return (
-    <ErrorBoundary error={error}>
-    <Router>
+    return (
+      <ErrorBoundary error={error}>
+        <Router>
           <Routes>
-              <Route path="/" element={ <HomePage /> } />
-              <Route element={ <NotFoundPage /> } />
+            <Route path="/" element={<HomePage />} />
+            <Route element={<NotFoundPage />} />
           </Routes>
-      </Router>
-    </ErrorBoundary>
-  )
+        </Router>
+      </ErrorBoundary>
+    );
 };
 
 function App() {
-  return (
-    <AuthProvider config={config}>
-      <AppContent />
-    </AuthProvider>
-  );
+    return (
+        <AuthProvider config={authConfig}>
+            <AppContent />
+        </AuthProvider>
+    );
 }
 
 export default App;
