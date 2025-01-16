81e4e13a0a0705e78a0f7b12ec941c311b15b25b
Pavindu Lakshan <pavindulakshan@gmail.com>
2025-01-16 18:56:20 +0530

    Load vite app configurations from environment variables

--- samples/asgardeo-react-app/vite.config.ts
+++ samples/asgardeo-react-app/vite.config.ts
@@ -1,32 +1,44 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv, UserConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import fs from 'fs';
 import path from 'path';
 
-export default defineConfig({
-  plugins: [
-    react()
-  ],
-  optimizeDeps: {
-    include: [
-      '@asgardeo/auth-react',
-      'react/jsx-runtime',
-      'react-router',
-      'react-router-dom',
-      'react',
-    ],
-  },
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'public', 'cert', 'localhost-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'public', 'cert', 'localhost-cert.pem')),
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd(), '')
+
+  // Explicitly define the configuration type
+  const config: UserConfig = {
+    plugins: [react()],
+    optimizeDeps: {
+      include: [
+        '@asgardeo/auth-react',
+        'react/jsx-runtime',
+        'react-router',
+        'react-router-dom',
+        'react',
+      ],
     },
-    host: 'localhost',
-    port: 3000
-  },
-  build: {
-    commonjsOptions: {
-      include: [ /react/ ],
-    }, 
+    server: {
+      host: env.HOST,
+      port: parseInt(env.PORT || '3000', 10),
+    },
+    build: {
+      commonjsOptions: {
+        include: [/react/],
+      },
+    },
+  };
+
+  if (env.HTTPS === 'true') {
+    config.server = {
+      ...config.server,
+      https: {
+        key: fs.readFileSync(path.resolve(__dirname, 'public', 'cert', 'localhost-key.pem')),
+        cert: fs.readFileSync(path.resolve(__dirname, 'public', 'cert', 'localhost-cert.pem')),
+      },
+    };
   }
-})
+
+  return config;
+});
